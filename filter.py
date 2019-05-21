b4d00c337a4f58c25ca8f1d9f18babe112b045d6
wangbo <804623282@qq.com>
2019-05-21 22:22:25 +0800

    initial

---
+++ filter.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+def is_odd(n):
+	return n % 2 == 1
+
+print(list(filter(is_odd, [1,2,3,4,5,6,7,8,9])))
+
+def not_empty(s):
+	return s and s.strip()
+
+print(list(filter(not_empty, ['A', ' ', 'bob', ' ', 'panglili'])))	
